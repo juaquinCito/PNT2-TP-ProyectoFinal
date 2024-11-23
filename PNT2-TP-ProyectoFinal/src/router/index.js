02c7482c0482cd6b3a49cac28d6eda2e3aabe4dc
Ernesto Gimenez <gmx90PT@gmail.com>
2024-11-23 16:59:18 -0300

    componentes  y rutas

--- PNT2-TP-ProyectoFinal/src/router/index.js
+++ PNT2-TP-ProyectoFinal/src/router/index.js
@@ -4,22 +4,68 @@ import LoginView from '../views/LoginView.vue';
 
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'login',
-            component: LoginView,
-        },
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '',
+      name: '',
+      component: MyTurns,
+      meta: { RequireAuth: true },
+    },
+    {
+      path: '',
+      name: '',
+      component: Edit,
+      meta: { RequireAuth: true },
+    },
+    {
+      path: '',
+      name: '',
+      component: HomeView,
+    },
+    {
+      path: '',
+      name: '',
+      component: Formulario,
+      meta: { RequireAuth: true },
+    },
+    {
+      path: '',
+      name: '',
+      component: AboutUs,
+    },
+    {
+      path: '',
+      name: '',
+      component: ContactUs,
+    },
+    {
+      path: '',
+      name: 'NotFound',
+      component: NotFound,
+    },
+    {
+      path: '',
+      name: '',
+      component: Carrito,
+      meta: { RequireAuth: true },
+    },
+
+
+  ],
 });
 router.beforeEach((to, from, next) => {
-    const store = useLoginStore();
-    if (to.matched.some((record) => record.meta.RequireAuth) && !store.isLogin) {
-        next('/');
-    } else {
-        next();
-    }
+  const store = useLoginStore();
+  if (to.matched.some((record) => record.meta.RequireAuth) && !store.isLogin) {
+    next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
