928b5d081e08ac877297aeb941c2b6423b336e09
Ernesto Gimenez <gmx90PT@gmail.com>
2024-11-23 03:09:50 -0300

    avance router, icono e imagen Inicio

--- PNT2-TP-ProyectoFinal/src/main.js
+++ PNT2-TP-ProyectoFinal/src/main.js
@@ -3,8 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 
-//import router from './router'
-//import Dialog from 'primevue/dialog';
+import router from './router'
+import Dialog from 'primevue/dialog';
 
 //import './style.css'
 //import 'bootstrap'
--- PNT2-TP-ProyectoFinal/src/router/index.js
+++ PNT2-TP-ProyectoFinal/src/router/index.js
@@ -1,5 +1,5 @@
-
-
+import { createRouter, createWebHistory } from 'vue-router';
+import { useLoginStore } from '../stores/login';
 
 
 
