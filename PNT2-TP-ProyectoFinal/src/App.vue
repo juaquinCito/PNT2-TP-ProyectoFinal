713270dfb1f1e2c287241c0132bc0d81d5436f19
Ernesto Gimenez <gmx90PT@gmail.com>
2024-11-24 20:22:14 -0300

    configuracion del app y componente noFound

--- PNT2-TP-ProyectoFinal/src/App.vue
+++ PNT2-TP-ProyectoFinal/src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
 import { storeToRefs } from "pinia";
+import {useLoginStore} from './stores/login'
 
+export default {
+     setup() {
+      const store= useLoginStore()
+      const { havePermissions } = store
+      const { isLogin, user } = storeToRefs(store)
+      return {isLogin, user, havePermissions}
+    }
+}
 </script>
 
 <template>
-  
+      <RouterLink to="/"></RouterLink>
+      <RouterLink to="/home"></RouterLink>
+      <RouterLink v-if="isLogin" to="/myTurns"></RouterLink>  
+      <RouterLink to="/contactus"></RouterLink>
+  <RouterView />
 </template>
 
 <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+  
+  body{
+    margin: 0;
+    font-family: 'Poppins', sans-serif;
+  }
 
 </style>
